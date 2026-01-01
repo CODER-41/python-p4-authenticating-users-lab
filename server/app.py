9871e92c27bdb239aa2c44ee6fe2c7f4d9a81fc9
RONNY <mboyaronny41@gmail.com>
2026-01-01 20:44:02 +0300

    authenticating -users-lab

--- server/app.py
+++ server/app.py
@@ -48,9 +48,57 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+# NEW: Login Resource
+class Login(Resource):
+    def post(self):
+        # Get username from request JSON
+        username = request.get_json().get('username')
+        
+        # Retrieve user by username
+        user = User.query.filter_by(username=username).first()
+        
+        if user:
+            # Set session's user_id to the user's id
+            session['user_id'] = user.id
+            
+            # Return user as JSON with 200 status code
+            return user.to_dict(), 200
+        
+        return {'error': 'User not found'}, 404
+
+# NEW: Logout Resource
+class Logout(Resource):
+    def delete(self):
+        # Remove user_id from session
+        session.pop('user_id', None)
+        
+        # Return no data with 204 status code
+        return '', 204
+
+# NEW: CheckSession Resource
+class CheckSession(Resource):
+    def get(self):
+        # Retrieve user_id from session
+        user_id = session.get('user_id')
+        
+        if user_id:
+            # If session has user_id, return the user as JSON with 200
+            user = User.query.filter_by(id=user_id).first()
+            if user:
+                return user.to_dict(), 200
+        
+        # If no user_id in session, return empty dict with 401 Unauthorized
+        return {}, 401
+
+# Register existing resources
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+
+# Register new login/logout/session resources
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
